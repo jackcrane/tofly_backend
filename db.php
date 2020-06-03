ddeb79fe4908b2dea2240d59f824fbc328b64c3c
jackcrane <3jbc22@gmail.com>
2020-06-03 11:32:04 -0400

    add the airport db and the first php script to iterate through it

--- db.php
+++ db.php
@@ -1,1 +1,10 @@
- 
+<?php
+
+$requested_airport = $_GET["icao"];
+
+$json  = '[{"var1":"9","var2":"16","var3":"16"},{"var1":"8","var2":"15","var3":"15"}]';
+$array = json_decode( $json, true );
+
+echo $array[$requested_airport];
+
+?>
