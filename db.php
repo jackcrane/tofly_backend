1dbfc3ddcd23a05d56b00f9e2c03f1063468c614
jackcrane <3jbc22@gmail.com>
2020-06-03 11:38:11 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 
 $requested_airport = $_GET["icao"];
-// 
-// var_dump($_GET);
 //
-// echo $requested_airport . "\n";
+var_dump($_GET);
+//
+echo "airpt: ".$requested_airport . "\n";
 
 $json  = file_get_contents("db.json");
 $array = json_decode( $json, true );
 
-echo $array[$requested_airport];
+var_dump($array);
 
 ?>
