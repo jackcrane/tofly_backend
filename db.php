a09cc54d35c4991f1acccdf5a2b2f6e7de736024
jackcrane <3jbc22@gmail.com>
2020-06-03 11:54:38 -0400

    Update db.php

--- db.php
+++ db.php
@@ -9,6 +9,59 @@ echo "airpt: ".$requested_airport . "\n";
 $json  = file_get_contents("db.json");
 $array = json_decode( $json, true );
 
-var_dump($array[strtoupper($requested_airport)]);
+$airport = $array[strtoupper($requested_airport)]
+
+// var_dump($array[strtoupper($requested_airport)]);
 
 ?>
+
+<html>
+
+  <table>
+    <tr>
+      <th>Header</th>
+      <th>Response</th>
+    </tr>
+    <tr>
+      <td>ICAO</td>
+      <td><?php echo $airport["icao"]; ?></td>
+    </tr>
+    <tr>
+      <td>IATA</td>
+      <td><?php echo $airport["iata"]; ?></td>
+    </tr>
+    <tr>
+      <td>Name</td>
+      <td><?php echo $airport["name"]; ?></td>
+    </tr>
+    <tr>
+      <td>City</td>
+      <td><?php echo $airport["city"]; ?></td>
+    </tr>
+    <tr>
+      <td>State</td>
+      <td><?php echo $airport["state"]; ?></td>
+    </tr>
+    <tr>
+      <td>Country</td>
+      <td><?php echo $airport["country"]; ?></td>
+    </tr>
+    <tr>
+      <td>Elevation</td>
+      <td><?php echo $airport["elevation"]; ?></td>
+    </tr>
+    <tr>
+      <td>lat</td>
+      <td><?php echo $airport["lat"]; ?></td>
+    </tr>
+    <tr>
+      <td>lon</td>
+      <td><?php echo $airport["lon"]; ?></td>
+    </tr>
+    <tr>
+      <td>TZ</td>
+      <td><?php echo $airport["tz"]; ?></td>
+    </tr>
+  </table>
+
+</html>
